bf3b38742628b1bc275cbe5727bf19a2cc5d8650
Kazee Siam <kzsiam@outlook.com>
2025-08-11 08:42:53 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.pzjaifg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req,res)=>{
     res.send('hello i am flight server')
